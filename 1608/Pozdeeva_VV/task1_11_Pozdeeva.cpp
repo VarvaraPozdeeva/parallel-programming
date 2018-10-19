0d7f1a5cb3d9d85394e27731248ac7b9a23ff001
Pozdeeva <varvarapozdeeva@gmail.com>
2018-10-19 20:26:36 +0300

    Add fixes

--- 1608/Pozdeeva_VV/task1_11_Pozdeeva.cpp
+++ 1608/Pozdeeva_VV/task1_11_Pozdeeva.cpp
@@ -4,43 +4,44 @@
 
 using namespace std;
 
-
-
 int main(int argc, char *argv[]) 
 {
-	//int **matrix = NULL;	// матрица	
 	int *matrix = nullptr;
 	int ProcNum, ProcRank;	
 	int N=0, M=0; // размерности матрицы 
-	int *result = NULL; // массив с результатом	
-	int *tmp =NULL;	
+	int *result = nullptr; // массив с результатом		
 	MPI_Status status;
 	double times;
-
+    int k = 0, l = 0;
 
 	MPI_Init(&argc, &argv);	
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
 
 	
-	if(ProcRank ==0) //заполняем матрицу и передаем другим процессам нужные строки 
+	if(ProcRank ==0) //заполняем матрицу
 	{
-        cout << "Enter matrix sizes.\nExample :\n5\n5" <<endl;
-       
-		cin >> N;
-		cin >> M;
+        if (argc < 3)
+        {
+            cout << "Enter Matrix Sizes\n N:" << endl;
+            cin >> N;
+            cout << "M:" << endl;
+            cin >> M;
+        }
+        else
+        {
+            N = atoi(argv[1]);
+            M = atoi(argv[2]);
+        }
 		
-		//matrix = new int*[N];
 		matrix = new int[N*M];
+        srand(time(0));
+        for (int i = 0; i<N*M; i++)
+            matrix[i] = rand() % 10;
+
 		result = new int[N];
- 		for(int i =0; i< N; i++)
-		{			
-			result[i] =0;
-		}		
-		srand(time(0));
-		for( int i=0; i<N*M; i++)
-				matrix[i] = rand() %10;
-		
+        for(int i =0; i< N; i++)
+			result[i] =0;		
 	}
 	
     //передаем размерности всем процессам
@@ -50,54 +51,67 @@ int main(int argc, char *argv[])
 	if (ProcRank == 0)
 	{
         //рассылаем строки матрицы по процессам
-		for(int i =0; i< N; i++)
-		{
-			if(i%ProcNum != 0)
-			{
-				MPI_Send(matrix, M, MPI_INT, i%ProcNum, i, MPI_COMM_WORLD);
-			}
-		}
+
+        k = N / ProcNum; // количество строк для каждого процесса
+        l = N % ProcNum; //оставшееся количество, первому процессу
+
+		for(int i =1; i< ProcNum; i++)
+            MPI_Send(matrix + (i)*k*M, k*M, MPI_INT, i, i, MPI_COMM_WORLD);
 	}
 	
+    times = MPI_Wtime();
+
     //вычисляем суммы, и возвращаем резултат в нулевой процесс
 	if(ProcRank != 0)
 	{
-		tmp = (int*) malloc(sizeof(int)*M);
-		int sum = 0;
-		for(int i= ProcRank; i < N; i+= ProcNum)
+        k = N / ProcNum;
+		int *tmp = new int[M*k];
+        int *res = new int[k];
+        int sum;
+       
+        MPI_Recv(tmp, M*k, MPI_INT, 0, ProcRank, MPI_COMM_WORLD, &status);
+        
+        for(int i=0; i < k; i++)
 		{
-			MPI_Recv(tmp, M, MPI_INT, 0, i, MPI_COMM_WORLD, &status);
-			for(int j=0; j< M; j++)
-			{
-				sum+= tmp[j];
-			}
-			MPI_Send(&sum, 1, MPI_INT, 0, i, MPI_COMM_WORLD);
-			
-		}
-		free(tmp);
-		
+            sum = 0;
+            for (int j = 0; j < M; j++)
+                sum += tmp[j + M*i];
+            res[i] = sum; 
+		}	
+        MPI_Send(res, k, MPI_INT, 0, ProcRank, MPI_COMM_WORLD);
+        delete tmp;
+        delete res;
 	}
 	else
-	{		
-		
-		int sum =0;
-		for(int  i = ProcRank; i< N ; i += ProcNum)
-		{
-			for(int j = 0; j< M; j++)
-				sum += matrix[i][j];
-			result[i] = sum;
-		}			
+	{	
+        int sum;
+        for (int i = 0; i < k; i++)
+        {
+            sum = 0;
+            for (int j = 0; j<M; j++)
+                sum += matrix[j+i*M];
+            result[i] = sum;
+        }
+        
+        if (l != 0)
+        {            
+            for (int i = 0; i < l; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < M; j++)
+                    sum += matrix[k*ProcNum*M + j + i*M];
+                result[i + k*ProcNum] = sum;
+            }
+        }
 	}
-		
+	
+    printf("Time of Proc # %d is  %.10f\n", ProcRank, MPI_Wtime() - times);    
 
 	if(ProcRank == 0)
 	{
-		for(int i =0; i< N; i++)
-			{
-				if(i%ProcNum != 0)
-				{
-					MPI_Recv(result + i, 1, MPI_INT, MPI_ANY_SOURCE, i , MPI_COMM_WORLD, &status);
-				}
+        for (int i = 1; i< ProcNum; i++)
+        {
+			MPI_Recv(result + i*k, k, MPI_INT, MPI_ANY_SOURCE, i, MPI_COMM_WORLD, &status);				
 		}
         //вывод матрицы и результатов
 		printf(" Matrix %dx%d \n", N, M);
@@ -106,23 +120,17 @@ int main(int argc, char *argv[])
 		{
 			for(int j =0; j<M; j++) 
 			{
-				printf("%d ", matrix[i][j]);
+				printf("%d ", matrix[i*M+j]);
 			}
 			printf("	sum = %d\n", result[i] );
 		}		 
-	}	
-	
-	MPI_Finalize();
-	cin >> N;
+    }	
 
-    if (matrix != NULL)
-    {
-        for (int i = 0; i < N; i++)
-            free(matrix[i]);
-        free(matrix);
-    }
-	if(result != NULL)
-        free(result);
-
+    if (matrix != nullptr)
+        delete matrix;
+    if (result != nullptr)
+        delete result;
+    
+    MPI_Finalize();
 	return 0;
 }
